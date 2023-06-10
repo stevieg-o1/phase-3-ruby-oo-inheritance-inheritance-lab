b6d3f084973d16072417752075aa249e1d3260c9
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-10 12:31:56 +0300

    completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "./user"
+
+class Student < User
+    attr_reader :knowledge
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(concept)
+        @knowledge << concept
+    end
+
 
 end
